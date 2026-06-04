e392571efc6a1c3c3b62a61def65fea2a780363e
Sara <sarakhalid.b28@gmail.com>
2026-06-04 10:24:36 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(str[39:66] + str[105:111] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
