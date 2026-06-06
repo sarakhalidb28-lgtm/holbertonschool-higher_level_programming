dfd62962cb6306569c0740b3291776de40287c48
Sara <sarakhalid.b28@gmail.com>
2026-06-06 20:54:11 +0300

    Create 100-my_calculator.py

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+cat << 'EOF' > 100-my_calculator.py
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    # التحقق من عدد المعاملات (اسم السكريبت + 3 معاملات = 4)
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    # التحقق من نوع العملية الحسابية وتمرير المتغيرات للدالة المناسبة
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # طباعة النتيجة بالشكل المطلوب
+    print("{} {} {} = {}".format(a, operator, b, result))
+EOF
