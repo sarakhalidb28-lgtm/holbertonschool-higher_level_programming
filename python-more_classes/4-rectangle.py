0638f832cd0ba0ef196561a4ea99ed81d06f2167
Sara <sarakhalid.b28@gmail.com>
2026-06-17 09:55:09 +0300

    Create 4-rectangle.py

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+هذا الموديل يحدد كلاس Rectangle مع إضافة التمثيل النصي الرسمي __repr__.
+"""
+
+
+class Rectangle:
+    """كلاس يمثل مستطيل يحتوي على جميع الخصائص، دوال الحساب، ووسائل التمثيل."""
+
+    def __init__(self, width=0, height=0):
+        """تهيئة مستطيل جديد بقيم افتراضية لعرضه وارتفاعه."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """جلب قيمة العرض (width)."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """تعيين قيمة العرض مع التحقق من النوع والقيمة."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """جلب قيمة الارتفاع (height)."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """تعيين قيمة الارتفاع مع التحقق من النوع والقيمة."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """حساب وإرجاع مساحة المستطيل."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """حساب وإرجاع محيط المستطيل."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """إرجاع تمثيل نصي للمستطيل باستخدام الرمز # للطباعة."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect_lines = ["#" * self.__width for _ in range(self.__height)]
+        return "\n".join(rect_lines)
+
+    def __repr__(self):
+        """إرجاع نص يمثل الكود البرمجي اللازم لإعادة إنشاء الكائن."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
